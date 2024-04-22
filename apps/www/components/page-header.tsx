8954f7ca923ed7c4448f4eeb6f288ad42ea13475
jeffrey <color.dove@gmail.com>
2024-04-22 17:03:17 +0800

    feat: update leading

--- apps/www/components/page-header.tsx
+++ apps/www/components/page-header.tsx
@@ -40,7 +40,7 @@ function PageHeaderDescription({
   return (
     <p
       className={cn(
-        "text-balance max-w-2xl text-lg font-light text-foreground",
+        "max-w-[750px] text-center text-lg leading-9 text-muted-foreground sm:text-xl",
         className
       )}
       {...props}
