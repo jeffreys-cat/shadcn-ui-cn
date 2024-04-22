5aa5c9771c0cea38bd2464a564fa97645c40c490
jeffrey <color.dove@gmail.com>
2024-04-22 16:56:03 +0800

    feat: add sitemap

--- apps/www/app/(app)/examples/layout.tsx
+++ apps/www/app/(app)/examples/layout.tsx
@@ -25,14 +25,14 @@ export default function ExamplesLayout({
     <>
       <PageHeader>
         <Announcement />
-        <PageHeaderHeading>Build your component library</PageHeaderHeading>
+        <PageHeaderHeading>构建你自己的组件库</PageHeaderHeading>
         <PageHeaderDescription>
           Beautifully designed components that you can copy and paste into your
           apps. Made with Tailwind CSS. Open source.
         </PageHeaderDescription>
         <PageActions>
           <Button asChild size="sm">
-            <Link href="/docs">Get Started</Link>
+            <Link href="/docs">快速开始</Link>
           </Button>
           <Button asChild size="sm" variant="ghost">
             <Link href="/blocks">Browse Blocks</Link>
